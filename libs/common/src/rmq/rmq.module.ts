9016ebb0acfe9e4c5e76e26a78b6a2d047fffd68
luvlqq <lualgod92@gmail.com>
2023-09-17 13:30:26 +0300

    feat: Add rmq resources

---
+++ libs/common/src/rmq/rmq.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { RmqService } from './rmq.service';
+
+@Module({
+  imports: [],
+  providers: [RmqService],
+  exports: [RmqService],
+})
+export class RmqModule {}
